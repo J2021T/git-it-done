1f0cd14f2c58cbc08e8cb00d89ed84f692819768
J2021T <80201567+J2021T@users.noreply.github.com>
2021-05-11 22:59:25 -0400

    added over 30 issues message

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,16 @@
+var limitWarningEl = document.querySelector('#limit-warning');
 var issueContainerEl = document.querySelector('#issues-container');
+
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = 'To see more than 30 issues, visit ';
+
+    var linkEl = document.createElement('a');
+    linkEl.textContent = 'See more Issues on GitHub.com';
+    linkEl.setAttribute('href', "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute('target', '_blank');
+
+    limitWarningEl.appendChild(linkEl);
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -6,6 +18,10 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                if (response.headers.get('Link')) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert('There was a problem with your request!');
